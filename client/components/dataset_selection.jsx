2fb9553a69f1cfab5f2c38bc6321a2bba039bf60
akodate <alex.kodate@gmail.com>
2016-09-23 01:42:08 -0700

    Add basic file uploading functionality

--- client/components/dataset_selection.jsx
+++ client/components/dataset_selection.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 
 const DatasetSelection = () => {
+  const uploadFile = (event) => {
+    const file = event.target.files[0]; 
+    console.log(file);
+    if (!file) return;
+
+    let xhr = new XMLHttpRequest(); 
+    xhr.open('POST', '/upload', true);
+    xhr.onload = (event) => {
+      console.log('done uploading!');
+    };
+
+    xhr.upload.onprogress = (event) => {
+      let percent = 100 * (event.loaded / event.total);
+      console.log(percent+'% uploaded');
+    };
+
+    xhr.send(file); 
+  };
+
   return (
     <div className='container dataset_selection_form'>
       <h1 className='text-center heading'>Select your cleaned dataset</h1>
       <form>
         <div className="form-group">
-          <input type="file" id="exampleInputFile" />
+          <input type="file" id="exampleInputFile" onChange={uploadFile} />
           <em className="help-block small">(CSV only. Max file size is **MB)</em>
         </div>
-      <button type="button" className="btn btn-lg btn-success center-block">Upload</button>
+      <button type="button" className="btn btn-lg btn-success center-block">Next</button>
       </form>
     </div>
   );
---
+++ server/main.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+// Saves file sent by file input in DataSelection component
+// TO DO: Create file and folder if they don't exist yet
+WebApp.connectHandlers.use('/upload',function(req,res){
+  const dataset_path = path.resolve('../../../../../datasets~/test.csv');
+  console.log("Writing dataset to", dataset_path);
+  var file = fs.createWriteStream(dataset_path); 
+
+  file.on('error',function(error){
+    console.log(error);
+  });
+  file.on('finish',function(){
+    res.writeHead(200) 
+    res.end();
+    console.log("Finished writing dataset.")
+  });
+
+  req.pipe(file);
+});
