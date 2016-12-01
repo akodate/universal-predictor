21a98da6f628a16a4482a3a974d89b87827eede6
akodate <alex.kodate@gmail.com>
2016-11-30 20:30:47 -0800

    Minor Python linting

--- imports/python_scripts/automl.py
+++ imports/python_scripts/automl.py
@@ -3,122 +3,122 @@ import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import LabelEncoder, label_binarize
+from sklearn.metrics import precision_recall_fscore_support, roc_curve, auc, confusion_matrix
 import autosklearn.classification
 from scipy.stats import mode
-from sklearn.metrics import precision_recall_fscore_support, roc_curve, auc, confusion_matrix
 import textwrap
 import json
 
 
 
 def encode_features(df, target):
-  predictor_types = []
-  for col in df.columns.difference([target]):
-    if not np.issubdtype(df[col], np.number):
-      predictor_types.append('categorical')
-      df[col] = LabelEncoder().fit_transform(df[col])
-    else:
-      predictor_types.append('numerical')
-  return df, predictor_types
+    predictor_types = []
+    for col in df.columns.difference([target]):
+        if not np.issubdtype(df[col], np.number):
+            predictor_types.append('categorical')
+            df[col] = LabelEncoder().fit_transform(df[col])
+        else:
+            predictor_types.append('numerical')
+    return df, predictor_types
 
 def format_info(csv_path, target, time, df, test_set_size, orig_dtypes):
-  info = """
-    ---INPUT---
-    CSV Path: {csv_path}
-    Target: {target}
-    Time: {time}
+    info = """
+        ---INPUT---
+        CSV Path: {csv_path}
+        Target: {target}
+        Time: {time}
 
-    Dataset shape: {df_shape}
-    Train/test split ratio: {train_set_size}:{test_set_size}
+        Dataset shape: {df_shape}
+        Train/test split ratio: {train_set_size}:{test_set_size}
 
-    Old dtypes / Converted dtypes
-    -----------------------------
-    {dtype_info}
-  """
+        Old dtypes / Converted dtypes
+        -----------------------------
+        {dtype_info}
+    """
 
-  return textwrap.dedent(info.format(
-    csv_path = csv_path,
-    target = target,
-    time = time,
-    df_shape = df.shape,
-    train_set_size = 1 - test_set_size,
-    test_set_size = test_set_size,
-    dtype_info = "\n    ".join([(col + ": " + old.name + " -> " + new.name) for col, old, new in zip(df.columns, orig_dtypes, df.dtypes)])
-  ))
+    return textwrap.dedent(info.format(
+        csv_path = csv_path,
+        target = target,
+        time = time,
+        df_shape = df.shape,
+        train_set_size = 1 - test_set_size,
+        test_set_size = test_set_size,
+        dtype_info = "\n    ".join([(col + ": " + old.name + " -> " + new.name) for col, old, new in zip(df.columns, orig_dtypes, df.dtypes)])
+    ))
 
 def format_results(score, predictions, y_test, probas):
-  results = """
-    ---RESULTS---
-    Score: {score}
-    Predictions (first 20):   {predictions}
-    Actual values (first 20): {y_test}
-    Prediction probabilities (first 5): 
-    {probas}
-  """
+    results = """
+        ---RESULTS---
+        Score: {score}
+        Predictions (first 20):   {predictions}
+        Actual values (first 20): {y_test}
+        Prediction probabilities (first 5): 
+        {probas}
+    """
 
-  return textwrap.dedent(results.format(
-    score = score,
-    predictions = predictions[0:20],
-    y_test = y_test[0:20],
-    probas = "\n    ".join([str(proba) for proba in probas[0:5]])
-  ))
+    return textwrap.dedent(results.format(
+        score = score,
+        predictions = predictions[0:20],
+        y_test = y_test[0:20],
+        probas = "\n    ".join([str(proba) for proba in probas[0:5]])
+    ))
 
 def get_ROC_values(class_names, y_test, probas):
-  encoded_class_names = range(len(class_names))
-  y_bin = label_binarize(y_test, classes=encoded_class_names)
+    encoded_class_names = range(len(class_names))
+    y_bin = label_binarize(y_test, classes=encoded_class_names)
 
-  if len(encoded_class_names) == 2:
-    fpr, tpr, roc_auc = get_binomial_ROC(y_bin, probas)
-  else:
-    fpr, tpr, roc_auc = get_polynomial_ROC(y_bin, probas)
-  return fpr, tpr, roc_auc
+    if len(encoded_class_names) == 2:
+        fpr, tpr, roc_auc = get_binomial_ROC(y_bin, probas)
+    else:
+        fpr, tpr, roc_auc = get_polynomial_ROC(y_bin, probas)
+    return fpr, tpr, roc_auc
 
 def get_binomial_ROC(y_bin, probas):
-  fpr, tpr, _ = roc_curve(y_bin, probas[:, 1])
-  fpr = fpr.tolist()
-  tpr = tpr.tolist()
-  roc_auc = auc(fpr, tpr)
-  return fpr, tpr, roc_auc
+    fpr, tpr, _ = roc_curve(y_bin, probas[:, 1])
+    fpr = fpr.tolist()
+    tpr = tpr.tolist()
+    roc_auc = auc(fpr, tpr)
+    return fpr, tpr, roc_auc
 
 def get_polynomial_ROC(y_bin, probas):
-  fpr = []
-  tpr = []
-  roc_auc = []
-  for i in range(len(y_bin[0])):
-    f, t, _ = roc_curve(y_bin[:, i], probas[:, i])
-    fpr.append(f.tolist())
-    tpr.append(t.tolist())
-    roc_auc.append(auc(f, t))
-  return fpr, tpr, roc_auc
+    fpr = []
+    tpr = []
+    roc_auc = []
+    for i in range(len(y_bin[0])):
+        f, t, _ = roc_curve(y_bin[:, i], probas[:, i])
+        fpr.append(f.tolist())
+        tpr.append(t.tolist())
+        roc_auc.append(auc(f, t))
+    return fpr, tpr, roc_auc
 
 def get_automl_ensemble_info(automl):
-  return [{
-      'model_name': get_model_name(model[1]), 
-      'configuration': get_model_configuration(model[1]),
-      'weight': automl.ensemble_.weights_[i]
-    }
-    for i, model in enumerate(automl.models_.items())]
+    return [{
+        'model_name': get_model_name(model[1]),
+        'configuration': get_model_configuration(model[1]),
+        'weight': automl.ensemble_.weights_[i]
+        }
+            for i, model in enumerate(automl.models_.items())]
 
 def get_model_name(model):
-  config = get_model_configuration(model)
-  try: 
-    return config['classifier:__choice__']
-  except:
-    return model.__class__.__name__ if not 'MyDummyClassifier' else 'dummy_classifier'
+    config = get_model_configuration(model)
+    try:
+        return config['classifier:__choice__']
+    except:
+        return model.__class__.__name__ if not 'MyDummyClassifier' else 'dummy_classifier'
 
 def get_model_configuration(model):
-  if type(model.configuration) != int:
-    return model.configuration.get_dictionary() 
-  else:
-    return {'code': model.configuration}
+    if type(model.configuration) != int:
+        return model.configuration.get_dictionary() 
+    else:
+        return {'code': model.configuration}
 
 def format_ensemble_treemap_data(ensemble_info):
-  return [{
-      'name': model_info['model_name'],
-      'value': model_info['weight'],
-      'configuration': model_info['configuration']
-    }
-    for i, model_info in enumerate(ensemble_info)]
+    return [{
+            'name': model_info['model_name'],
+            'value': model_info['weight'],
+            'configuration': model_info['configuration']
+        }
+        for i, model_info in enumerate(ensemble_info)]
 
 
 
@@ -140,8 +140,8 @@ y = LabelEncoder().fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_set_size)
 
 automl = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=time, 
-                                                          per_run_time_limit=int(time / 10), 
-                                                          ml_memory_limit=10000)
+                                                                                                                    per_run_time_limit=int(time / 10), 
+                                                                                                                    ml_memory_limit=10000)
 automl.fit(X_train, y_train, feat_type=predictor_types)
 
 score = automl.score(X_test,y_test)
@@ -155,21 +155,21 @@ ensemble_info = get_automl_ensemble_info(automl)
 
 print("***JSON***", flush=True)
 print(json.dumps({
-  'info_log': format_info(csv_path, target, time, df, test_set_size, orig_dtypes),
-  'results_log': format_results(score, predictions, y_test, probas),
-  'results': {
-    'score': score,
-    'baselineAcc': baseline_acc,
-    'trueValues': y_test.tolist(),
-    'predictions': predictions.tolist(),
-    'probas': probas.tolist(),
-    'classNames': class_names.tolist(),
-    'ensembleInfo': ensemble_info,
-    'formattedEnsembleTreemapData': format_ensemble_treemap_data(ensemble_info),
-    'precRecFscoreSupport': np.rot90(np.fliplr(precRecFscoreSupport)).tolist(),
-    'cnfMatrix': np.rot90(np.fliplr(confusion_matrix(y_test, predictions))).tolist(),
-    'fpr': fpr,
-    'tpr': tpr,
-    'rocAUC': roc_auc
-  }
+    'info_log': format_info(csv_path, target, time, df, test_set_size, orig_dtypes),
+    'results_log': format_results(score, predictions, y_test, probas),
+    'results': {
+        'score': score,
+        'baselineAcc': baseline_acc,
+        'trueValues': y_test.tolist(),
+        'predictions': predictions.tolist(),
+        'probas': probas.tolist(),
+        'classNames': class_names.tolist(),
+        'ensembleInfo': ensemble_info,
+        'formattedEnsembleTreemapData': format_ensemble_treemap_data(ensemble_info),
+        'precRecFscoreSupport': np.rot90(np.fliplr(precRecFscoreSupport)).tolist(),
+        'cnfMatrix': np.rot90(np.fliplr(confusion_matrix(y_test, predictions))).tolist(),
+        'fpr': fpr,
+        'tpr': tpr,
+        'rocAUC': roc_auc
+    }
 }), flush=True)
--- imports/python_scripts/df_describe.py
+++ imports/python_scripts/df_describe.py
@@ -1,51 +1,51 @@
 import sys
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, label_binarize
+from sklearn.preprocessing import LabelEncoder
 import textwrap
 import json
 
 
 
 def format_info(csv_path, df, test_set_size, orig_dtypes):
-  info = """
-    ---INPUT---
-    CSV Path: {csv_path}
+    info = """
+        ---INPUT---
+        CSV Path: {csv_path}
 
-    Dataset shape: {df_shape}
-    Train/test split ratio: {train_set_size}:{test_set_size}
+        Dataset shape: {df_shape}
+        Train/test split ratio: {train_set_size}:{test_set_size}
 
-    Old dtypes / Converted dtypes
-    -----------------------------
-    {dtype_info}
-  """
+        Old dtypes / Converted dtypes
+        -----------------------------
+        {dtype_info}
+    """
 
-  return textwrap.dedent(info.format(
-    csv_path = csv_path,
-    df_shape = df.shape,
-    train_set_size = 1 - test_set_size,
-    test_set_size = test_set_size,
-    dtype_info = "\n    ".join([(col + ": " + old.name + " -> " + new.name) for col, old, new in zip(df.columns, orig_dtypes, df.dtypes)])
-  ))
+    return textwrap.dedent(info.format(
+        csv_path = csv_path,
+        df_shape = df.shape,
+        train_set_size = 1 - test_set_size,
+        test_set_size = test_set_size,
+        dtype_info = "\n    ".join([(col + ": " + old.name + " -> " + new.name) for col, old, new in zip(df.columns, orig_dtypes, df.dtypes)])
+    ))
 
 def generate_df_description_table(df_description, orig_dtypes):
-  df_description_table = [row.tolist() for row in df_description.fillna(value='').as_matrix()]
-  [df_description_table[i].insert(0, row_name) for i, row_name in enumerate(df_description.index)]
-  df_description_table.insert(0, ['TYPE'] + [dtype.name for dtype in orig_dtypes])
-  df_description_table.insert(0, [''] + df_description.columns.tolist())
-  df_description_table = [[np_to_python(value) for value in row] for row in df_description_table]
-  return df_description_table
+    df_description_table = [row.tolist() for row in df_description.fillna(value='').as_matrix()]
+    [df_description_table[i].insert(0, row_name) for i, row_name in enumerate(df_description.index)]
+    df_description_table.insert(0, ['TYPE'] + [dtype.name for dtype in orig_dtypes])
+    df_description_table.insert(0, [''] + df_description.columns.tolist())
+    df_description_table = [[np_to_python(value) for value in row] for row in df_description_table]
+    return df_description_table
 
 def np_to_python(value):
-  if isinstance(value, np.generic):
-    return np.asscalar(value)
-  return value
+    if isinstance(value, np.generic):
+        return np.asscalar(value)
+    return value
 
 def encode_binary_features(df):
-  for col in df.columns:
-    if len(df[col].unique()) == 2:
-      df[col] = LabelEncoder().fit_transform(df[col])
-  return df
+    for col in df.columns:
+        if len(df[col].unique()) == 2:
+            df[col] = LabelEncoder().fit_transform(df[col])
+    return df
 
 
 
@@ -59,10 +59,10 @@ test_set_size = 0.2
 print("Description follows:")
 print("***JSON***", flush=True)
 print(json.dumps({
-  'info_log': format_info(csv_path, df, test_set_size, orig_dtypes),
-  'results_log': "",
-  'results': {
-    'dfDescription': df_description_table,
-    'corrMatrix': encode_binary_features(df).corr().as_matrix().tolist()
-  }
+    'info_log': format_info(csv_path, df, test_set_size, orig_dtypes),
+    'results_log': "",
+    'results': {
+        'dfDescription': df_description_table,
+        'corrMatrix': encode_binary_features(df).corr().as_matrix().tolist()
+    }
 }), flush=True)
